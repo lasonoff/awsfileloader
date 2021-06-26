b3945ca33bddfd0f6f015404b423e2e22f83c114
lasonoff <lason-vrn@yandex.ru>
2021-06-26 23:02:46 +0300

    Initial commit

---
+++ src/test/java/ru/yauroff/awsfileloader/AwsfileloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yauroff.awsfileloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsfileloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
